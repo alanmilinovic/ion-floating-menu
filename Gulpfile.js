4d934d7f4ad9be3b01de16452491f41235af58e5
Alan <amilino@gmail.com>
2025-02-04 12:45:16 +0100

    Update Gulpfile.js

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,30 +2,26 @@ const gulp = require('gulp');
 const uglify = require('gulp-uglify');
 const concat = require('gulp-concat');
 const cleanCSS = require('gulp-clean-css');
+const log = require('fancy-log');
 
 // Minify & concatenate JavaScript
 function minifyJS() {
-    return gulp.src('./dist/*.js') // Source JS files
-        .pipe(uglify()) // Minify JS
-        .pipe(concat('ion-floating-menu.min.js')) // Concatenate
-        .pipe(gulp.dest('dist/')); // Save output
+    return gulp.src('./dist/*.js')
+        .pipe(uglify())
+        .pipe(concat('ion-floating-menu.min.js'))
+        .pipe(gulp.dest('dist/'))
+        .on('end', () => log('✔️ JS Minified!'));
 }
 
 // Minify & concatenate CSS
 function minifyCSS() {
-    return gulp.src('./dist/*.css') // Source CSS files
-        .pipe(cleanCSS({ compatibility: 'ie8' })) // Minify CSS
-        .pipe(concat('ion-floating-menu.min.css')) // Concatenate
-        .pipe(gulp.dest('dist/')); // Save output
+    return gulp.src('./dist/*.css')
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
+        .pipe(concat('ion-floating-menu.min.css'))
+        .pipe(gulp.dest('dist/'))
+        .on('end', () => log('✔️ CSS Minified!'));
 }
 
-// Watch for changes (Optional)
-function watchFiles() {
-    gulp.watch('./dist/*.js', minifyJS);
-    gulp.watch('./dist/*.css', minifyCSS);
-}
-
-// Define tasks
+// Define build task
 exports.build = gulp.series(minifyJS, minifyCSS);
-exports.watch = gulp.series(exports.build, watchFiles);
 exports.default = exports.build;

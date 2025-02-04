123547997b3deb26a9cc943f523a3577f551bac8
Alan <amilino@gmail.com>
2025-02-04 13:45:51 +0100

    Update Gulpfile.js

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,12 +3,12 @@ const uglify = require('gulp-uglify');
 const concat = require('gulp-concat');
 const cleanCSS = require('gulp-clean-css');
 const log = require('fancy-log');
-import { deleteAsync as del } from 'del';
+const del = require('del'); // ✅ Now works with CommonJS
 
 // Clean old minified files
-async function clean() {
-    await deleteAsync(['dist/ion-floating-menu.min.js', 'dist/ion-floating-menu.min.css']);
-    log('🗑️  Old minified files removed!');
+function clean() {
+    return del(['dist/ion-floating-menu.min.js', 'dist/ion-floating-menu.min.css'])
+        .then(() => log('🗑️  Old minified files removed!'));
 }
 
 // Minify & concatenate JavaScript
